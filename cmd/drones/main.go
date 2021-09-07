93765bf350e70f81a17996049486366f3ed4e365
mmuoDev <radioactive.uche11@gmail.com>
2021-09-07 22:31:23 +0100

    completed main.go

--- cmd/drones/main.go
+++ cmd/drones/main.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"drones/internal/app"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	a := app.New()
+	log.Println(fmt.Sprintf("Starting server on port:%s", port))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), a.Handler()))
+}
